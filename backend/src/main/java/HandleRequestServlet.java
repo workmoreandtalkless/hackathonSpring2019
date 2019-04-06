e9a7da9ed44dc6bb10ed3d6806529698c09cd68f
Zhenhua Cai <steven.cai1990@hotmail.com>
2019-04-06 09:59:22 -0400

    add category

--- backend/src/main/java/HandleRequestServlet.java
+++ backend/src/main/java/HandleRequestServlet.java
@@ -11,9 +11,9 @@ import java.io.IOException;
         urlPatterns = "/HandleRequestServlet"
 )
 public class HandleRequestServlet extends HttpServlet {
-    private String[] answers = new String[3];
-    private static String[] questions = new String[3];
-    private static String[][] options = new String[3][];
+    private String[] answers = new String[4];
+    private static String[] questions = new String[4];
+    private static String[][] options = new String[4][];
     {
         initQuestions();
     }
@@ -23,7 +23,7 @@ public class HandleRequestServlet extends HttpServlet {
         if(value!=0) answers[value-1]=ans;
         HttpSession session = request.getSession();
         session.setAttribute("answers", answers);
-        if(value<3){
+        if(value<4){
             request.setAttribute("stage", value+1);
             request.setAttribute("question", questions[value]);
             request.setAttribute("options", options[value]);
@@ -38,24 +38,30 @@ public class HandleRequestServlet extends HttpServlet {
     }
     private static void initQuestions(){
         questions[0]="Which block do you want?";
-        questions[1]="What's the total amount do you want to invest?";
-        questions[2]="How much do you want to pay rent?";
+        questions[1]="Which category do you want?";
+        questions[2]="What's the total amount do you want to invest?";
+        questions[3]="How much do you want to pay rent?";
         options[0]=new String[3];
         options[0][0]="Flushing";
         options[0][1]="Bayside";
         options[0][2]="Woodside";
-        options[1]=new String[6];
-        options[1][0]="Less than $10,000";
-        options[1][1]="$10,000 - $100,000 ";
-        options[1][2]="$100,000 - $300,000";
-        options[1][3]="$300,000 - $500,000";
-        options[1][4]="$500,000 - $1,000,000";
-        options[1][5]="Over $100,000";
 
-        options[2]= new String[3];
+        options[1]=new String[2];
+        options[1][0]="Coffee";
+        options[1][1]="Gym";
+
+        options[2]=new String[6];
         options[2][0]="Less than $10,000";
-        options[2][1]="$10,000 - $20,000";
-        options[2][2]="Over $20,000";
+        options[2][1]="$10,000 - $100,000 ";
+        options[2][2]="$100,000 - $300,000";
+        options[2][3]="$300,000 - $500,000";
+        options[2][4]="$500,000 - $1,000,000";
+        options[2][5]="Over $100,000";
+
+        options[3]= new String[3];
+        options[3][0]="Less than $10,000";
+        options[3][1]="$10,000 - $20,000";
+        options[3][2]="Over $20,000";
 
     }
 }
